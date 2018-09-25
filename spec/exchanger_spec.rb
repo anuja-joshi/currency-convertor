cf7a8f1cf86a242e1b53238a8d119d6caa0142e3
Ninad <ninadnehete@gmail.com>
2018-09-25 14:09:32 +0530

    Added xit to pending test cases

--- spec/exchanger_spec.rb
+++ spec/exchanger_spec.rb
@@ -1,14 +1,14 @@
 describe 'Exchanger' do
   context 'Positives test cases' do
-    it 'converts 1 USD to INR' do
+    xit 'converts 1 USD to INR' do
     end
   end
 
   context 'Negative test cases' do
-    it 'raises unknown currency error when currency code is incorrect' do
+    xit 'raises unknown currency error when currency code is incorrect' do
     end
 
-    it 'raises incorrect currency value error when numeric input is \
+    xit 'raises incorrect currency value error when numeric input is \
     not added' do
     end
   end
